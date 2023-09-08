026f026b3c483ac12b1035519ba87fdf36c53883
SarahAndDuck <ratkevirina@gmail.com>
2023-09-08 22:32:30 +0700

    feat: add solution for brackets.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let arr = str.split("");
+  let flag = true;
+  while (flag) {
+    flag = false;
+    for (let i = 0; i < str.length; i++)
+      for (let j = 0; j < bracketsConfig.length; j++) {
+        if (
+          arr[i] === bracketsConfig[j][0] &&
+          arr[i + 1] === bracketsConfig[j][1]
+        ) {
+          arr.splice(i, 2);
+          flag = true;
+        }
+      }
+  }
+  return arr.length === 0;
+};
